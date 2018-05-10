4e487ba4e3a1e67e0aa6b107da54ab901e23d5d5
Ederson Torresini <boidacarapreta@gmail.com>
2018-05-10 10:02:02 -0300

    Criação de diretório de socket.

--- samba/nmbd/docker-entrypoint.sh
+++ samba/nmbd/docker-entrypoint.sh
@@ -6,4 +6,7 @@ if [ -f /var/lib/samba/wins.dat ]
 then
     rm -f /var/lib/samba/wins.dat
 fi
+mkdir -p /var/lib/samba/private/msg.sock
+chmod 0700 /var/lib/samba/private/msg.sock
+
 exec "$@" < /dev/null
