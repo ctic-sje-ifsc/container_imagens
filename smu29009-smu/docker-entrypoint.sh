5176fb2751aaee9bdee2606aebb0df8656a79aa2
Gabriel de Souza <souzagaabriel@localhost.localdomain>
2018-06-25 23:50:57 -0300

    Ambos tem que mudar para o domínio para funcionar.

--- smu29009-smu/docker-entrypoint.sh
+++ smu29009-smu/docker-entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 sh -c '/bin/sed -i "s/localhost/etherpad.sj.ifsc.edu.br/g" /usr/src/app/video-conference-webrtc/server.js'
-sh -c '/bin/sed -i "s/127.0.0.1/191.36.8.25/g" /usr/src/app/video-conference-webrtc/server.js'
+sh -c '/bin/sed -i "s/127.0.0.1/etherpad.sj.ifsc.edu.br/g" /usr/src/app/video-conference-webrtc/server.js'
 
 
 exec "$@"
