dbc514fda3b8d091f084b5284797ef82bcf29822
Gabriel de Souza <souzagaabriel@gmail.com>
2018-08-10 13:08:01 -0300

    Criar o atalho e instalar o pacote libasound2.

--- matlab/docker-entrypoint.sh
+++ matlab/docker-entrypoint.sh
@@ -5,4 +5,7 @@ set -e
 # SSH login fix. Otherwise user is kicked off after login
 sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd
 
+# Cria um link no /usr/local/bin
+ln -s /opt/MATLAB/R2015a/bin/matlab /usr/local/bin/matlab
+
 exec "$@"
