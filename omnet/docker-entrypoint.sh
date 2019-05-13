fe422381923f6738eca594938e2be951109832cb
souzagaabriel <souzagaabriel@gmail.com>
2019-05-13 11:15:41 -0300

    Configura X11UseLocalhost no no omnet.

--- omnet/docker-entrypoint.sh
+++ omnet/docker-entrypoint.sh
@@ -13,5 +13,6 @@ cat /mnt/shadow >> /etc/shadow
 cat /mnt/group >> /etc/group
 /bin/bash /mnt/cron_att_users.sh &
 /bin/bash /mnt/corrige_permissao.sh &
+echo "X11UseLocalhost no" >> /etc/ssh/sshd_config
 
 exec "$@"
