46fd99dc5e913280b947ba22c45a2f4ca46f93c1
souzagaabriel <souzagaabriel@gmail.com>
2019-05-13 10:55:27 -0300

    Adiciona opção X11UseLocalhost no matlab.

--- matlab/docker-entrypoint.sh
+++ matlab/docker-entrypoint.sh
@@ -12,5 +12,6 @@ cat /mnt/passwd >> /etc/passwd
 cat /mnt/shadow >> /etc/shadow
 cat /mnt/group >> /etc/group
 /bin/bash /mnt/cron_att_users.sh &
+echo "X11UseLocalhost no" >> /etc/ssh/sshd_config
 
 exec "$@"
