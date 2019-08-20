a7591e2fad290ee6feb0608c4c4860235eeaa667
ricardoifsc <ricardo.ifsc@gmail.com>
2019-08-20 17:36:13 -0300

    Atualizacao do fog para 1.5.7

--- fog/docker-entrypoint.sh
+++ fog/docker-entrypoint.sh
@@ -2,10 +2,12 @@
 
 set -e
 
-sh -c '/bin/sed -i "s/^Listen 80.*/Listen 8083/g" /etc/apache2/ports.conf' 
+sh -c '/bin/sed -i "s/^Listen 80.*/Listen 8083/g" /etc/apache2/ports.conf'
 sh -c '/bin/sed -i "s/Listen 443$/Listen 44383/g" /etc/apache2/ports.conf'
 sh -c '/bin/sed -i "1s/.*/<VirtualHost *>/g" /etc/apache2/sites-enabled/001-fog.conf'
 sh -c '/bin/sed -i "s/191.36.8.25/191.36.8.25:8083/g" /tftpboot/default.ipxe'
+sh -c '/bin/sed -i "s/ServerName.*/ServerName 191.36.8.25/g" /etc/apache2/sites-available/001-fog.conf'
+sh -c "/bin/sed -i '/HOST/ s/\".*\"/\"191.36.8.25\"/g' /var/www/fog/lib/fog/config.class.php"
 sh -c '/etc/init.d/start.services'
 
 exec "$@"
