14645645fae306448cd85dbe2450281489d15984
souzagaabriel <souzagaabriel@gmail.com>
2019-09-10 11:06:15 -0300

    Atualiza container Eagle.

--- eagle/docker-entrypoint.sh
+++ eagle/docker-entrypoint.sh
@@ -8,5 +8,6 @@ cat /mnt/group >> /etc/group
 /bin/bash /mnt/cron_att_users.sh &
 /bin/bash /mnt/corrige_permissao.sh &
 echo "X11UseLocalhost no" >> /etc/ssh/sshd_config
+chmod -R o+rx /opt/*
 
 exec "$@"
