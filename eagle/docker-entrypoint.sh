678715b6d2bab4449ffa02ba8a567afffa66b2e0
souzagaabriel <souzagaabriel@gmail.com>
2019-09-24 07:59:39 -0300

    Remove linha que não era desse container.

--- eagle/docker-entrypoint.sh
+++ eagle/docker-entrypoint.sh
@@ -6,7 +6,6 @@ cat /mnt/passwd >> /etc/passwd
 cat /mnt/shadow >> /etc/shadow
 cat /mnt/group >> /etc/group
 /bin/bash /mnt/cron_att_users.sh &
-/bin/bash /mnt/corrige_permissao.sh &
 echo "X11UseLocalhost no" >> /etc/ssh/sshd_config
 chmod -R o+rx /opt/*
 
